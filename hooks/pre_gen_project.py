a09c895197f3b3e2fba43aa298d79c45a97e7825
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2019-10-22 14:20:57 +0100

    Added check for repo_name to ensure it's a valid Python module name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,6 +3,8 @@
 
 import re
 import sys
+# from pathlib import Path
+# import json
 
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
@@ -14,3 +16,6 @@ if not re.match(MODULE_REGEX, module_name):
 
     # exits with status 1 to indicate failure
     sys.exit(1)
+
+# TODO Check user-level settings for conda and VSCode, and update if needed
+# Should fail if can't do update
