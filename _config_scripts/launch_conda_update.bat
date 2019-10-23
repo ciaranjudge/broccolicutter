9cbc1c3a73a73e7a13643ec9915fa14f5cf0fe93
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2019-10-23 16:29:22 +0100

    Fixed paths following folder name change for _config_scripts.

--- _config_scripts/launch_conda_update.bat
+++ _config_scripts/launch_conda_update.bat
@@ -3,5 +3,5 @@ echo --------------------------------------------------------
 echo                  *** DON'T PANIC! ***
 echo --------------------------------------------------------
 call %LOCALAPPDATA%\Continuum\miniconda3\Scripts\activate.bat
-call python .\_config_scripts\update_conda.py
+call python ".\{{ cookiecutter.repo_name }}\_config_scripts\update_conda.py"
 
